1af46eb67427e62dc6071cd55305796f89476799
Daniel Elliot <danielelliotr@gmail.com>
2021-02-10 11:42:40 -0800

    Post route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app           = express();
 
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+require('./app/routes')(app, {});
+
 app.listen(port, () => {
   console.log(`We are live on Port ${port}`);
 })
